89b3b781ad05dedc68cd5dd4994514d91420743f
Dominik Porada <dominik.porada@monterail.com>
2013-05-04 18:05:07 +0200

    Initial commit

---
+++ config.rb
@@ -0,0 +1,20 @@
+# Asset configuration
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+
+# Make Haml use double quotes
+set :haml, { attr_wrapper: '"' }
+
+# Development-specific configuration
+configure :development do
+  # Enable Sass source maps
+  set :sass, { :debug_info => true }
+end
+
+# Build-specific configuration
+configure :build do
+  # Minify assets
+  activate :minify_css
+  activate :minify_javascript
+end
