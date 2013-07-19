1396640b899f3b6203ae5da69b8c576c760d8584
Dominik Porada <dominik@porada.co>
2013-07-20 01:43:04 +0200

    Disable Sass’ deprecated source maps

--- config.rb
+++ config.rb
@@ -23,9 +23,6 @@ end
 # Development-specific configuration
 configure :development do
   activate :livereload
-
-  # Enable Sass source maps
-  set :sass, { :debug_info => true }
 end
 
 # Build-specific configuration
